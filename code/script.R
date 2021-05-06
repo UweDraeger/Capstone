6621a9fc9708ae5671367363428bdc79dd7f65cb
UweDraeger <uwe.draeger@gmx.net>
2021-05-06 15:36:17 +0200

    more EDA

--- code/script.R
+++ code/script.R
@@ -1,22 +1,44 @@
 library(tidyverse)
 library(tidytext)
 
-setwd("~/Coursera/Scripts and Data/Capstone/Capstone")
+setwd("~/Coursera/Scripts and Data/Capstone")
 
 data_dir <- file.path(getwd(), "datasets", "final", "en_US")
-file_name <- "en_US.blogs.txt"
 
-en_blogs <- read_lines(paste0(data_dir,"/", file_name),
-                       n_max = 500,
+tw_file_name <- "en_US.twitter.txt"
+bl_file_name <- "en_US.blogs.txt"
+nw_file_name <- "en_US.news.txt"
+
+twitter_data <- read_lines(paste0(data_dir,"/", tw_file_name),
+                           n_max = 5000,
+                           skip_empty_rows = TRUE)
+blogs_data <- read_lines(paste0(data_dir,"/", bl_file_name),
+                       skip_empty_rows = TRUE)
+news_data <- read_lines(paste0(data_dir,"/", nw_file_name),
                        skip_empty_rows = TRUE)
 
-text_df <- tibble(line = 1:length(en_blogs), text = en_blogs)
+tw_text <- tibble(line = 1:length(twitter_data), text = twitter_data)
+bl_text <- tibble(line = 1:length(blogs_data), text = blogs_data)
+nw_text <- tibble(line = 1:length(news_data), text = news_data)
 
 # tokenization - separate into words 
-text_df %>%
+tw_words <- tw_text %>%
         unnest_tokens(word, text) %>%
-        anti_join(stop_words) %>%
-        count(word, sort = TRUE) %>%
-        filter(n > 15) %>%
-        mutate(word = reorder(word, n)) %>%
-        ggplot(aes(n, word)) + geom_col() + labs(y = NULL)
+        count(word, sort = TRUE)
+
+bl_words <- bl_text %>%
+        unnest_tokens(word, text) 
+        anti_join(stop_words)
+
+nw_words <- nw_text %>%
+        unnest_tokens(word, text) 
+        anti_join(stop_words)
+        
+# tokenization - separate into ngrams
+tw_2grams <- tw_text %>%
+        unnest_tokens(ngram, text, token = "ngrams", n = 2)  %>%
+        count(ngram, sort = TRUE)
+
+tw_3grams <- tw_text %>%
+        unnest_tokens(ngram, text, token = "ngrams", n = 3)  %>%
+        count(ngram, sort = TRUE)
